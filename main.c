f5f6cce1d65bb4a2e4d380660c5877f208194b95
dmike16 <cipmiky@gmail.com>
2014-03-13 23:13:46 +0100

    zip all the new calls...

--- main.c
+++ main.c
@@ -20,13 +20,11 @@ int main(void)
     omega[i][1]=3.00f;
     }
   printf("Hello World!\n");
-  void* grid = new(Grid,3,100,omega);
-  void *sphere = new(SurfaceSphere,grid,3,1.00f);
-  void *surf = new(ImplicitForm, sphere,0.5);
+  void *surf = new(ImplicitForm,
+                   new(SurfaceSphere,
+                       new(Grid,3,100,omega),3,1.00f),0.5);
   printf("Il valore della superfici nel punto %.2f,%.2f,%.2f è %.2f\n",
          0.4f,0.6f,0.3f,u_surface(surf,x));
-  delete(grid);
-  delete(sphere);
   delete(surf);
   for(i=0; i<3; i++)
     free(omega[i]);
