a0328dcce9971fa5d0d64135b535b49bbd712d1f
dmike16 <cipmiky@gmail.com>
2014-03-14 23:36:37 +0100

    put the files in directories

---
+++ lib/include/Object.h
@@ -0,0 +1,25 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#ifndef Method_Type
+#define Method_Type
+
+typedef void (*Method) ();
+
+#endif
+
+extern const void *_Object;
+#define Object (_Object ? _Object : (_Object = __object()))
+
+struct __Object *new (const void *class,...);
+void *allocate(const void *self);
+void delete (void *self);
+int puto (const void *self,FILE *fp);
+const void* __object();
+const void* __class();
+Method respondsTo(const void *self, const char *tag);
+
+extern const void *_Class;
+#define Class (_Class ? _Class : (_Class = __class()))
+
+#endif /* header Object.h */
---
+++ lib/include/Object.r
@@ -0,0 +1,54 @@
+#ifndef OBJECT_R
+#define OBJECT_R
+
+#ifndef Method_Type
+#define Method_Type
+
+typedef void (*Method) ();
+
+#endif
+
+typedef void * (*ctoR) (void*,va_list*);
+typedef struct __Object * (*neW) (const void*,va_list*);
+typedef void * (*dtoR) (void*);
+typedef int (*putO)(const void*,FILE*);
+
+struct __Object {
+       unsigned long magic;
+       const struct __Class *class;
+}; 
+
+struct Method {
+  const char *tag;
+  Method selector;
+  Method method;
+};
+
+struct __Class {
+       const struct __Object _;
+       const char *name;
+       const struct __Class *super;
+       size_t size;
+       struct Method ctor;
+       struct Method dtor;
+       struct Method puto;
+       struct Method delete;
+       struct Method new;
+};
+
+void *ctor(void* self, va_list *app);
+void *dtor(void *self);
+const void *classOf (const void *self);
+const void *super (const void *self);
+size_t sizeOf (const void *self);
+void *super_ctor(const void *class, void *self, va_list *app);
+void *super_dtor(const void *class, void *self);
+void *super_new(const void *class, const void *self, va_list *app);
+void *super_delete(const void *class, void *self);
+int super_puto(const void *class,const void *self, FILE *fp);
+int isA (const void *self, const struct __Class *class);
+int isOf (const void *self, const struct __Class *class);
+void *cast (const struct __Class *class,const void *self);
+
+
+#endif /* header Object.h */
